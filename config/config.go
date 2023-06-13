9f6007cd4f8bd696a35c2b2c6392edf29fe3b159
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-06-13 21:49:25 +0300

    config structure rectified

--- config/config.go
+++ config/config.go
@@ -3,18 +3,12 @@ package config
 import "math/big"
 
 type Configurations struct {
-	Local			NetworkConfiguration
-	ETHTestnet		NetworkConfiguration
-	ETHMainnet		NetworkConfiguration
-	POLYTestnet		NetworkConfiguration
-	POLYMainnet		NetworkConfiguration
-	AVATestnet		NetworkConfiguration
-	AVAMainnet		NetworkConfiguration
+	Networks map[string]map[string]NetworkConfiguration `mapstructure:"networks"`
 }
 
 type NetworkConfiguration struct {
-	PrivateKey		string
-	Network			string
-	DeployedAddress	string
-	ChainId			*big.Int
+	PrivateKey      string
+	Network         string
+	DeployedAddress string
+	ChainId         *big.Int
 }
