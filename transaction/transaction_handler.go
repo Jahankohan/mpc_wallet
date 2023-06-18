cd50cef1895b3a1bb9009b8c2dcd954a013ca4a6
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-06-19 00:41:32 +0300

    Transaction package refined

--- transaction/transaction_handler.go
+++ transaction/transaction_handler.go
@@ -2,37 +2,32 @@ package transaction
 
 import (
 	"log"
+	"math/big"
 
 	"github.com/Jahankohan/mpc_wallet/config"
+	"github.com/Jahankohan/mpc_wallet/key_manager"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type TransactionHandler struct {
-	creator    TransactionCreator
-	signer     TransactionSigner
+	builder     *TransactionBuilder
 	broadcaster TransactionBroadcaster
 }
 
-func NewTransactionHandler(creator TransactionCreator, signer TransactionSigner, broadcaster TransactionBroadcaster) *TransactionHandler {
+func NewTransactionHandler(client *ethclient.Client, keyManager *key_manager.KeyManager, broadcaster TransactionBroadcaster) *TransactionHandler {
 	return &TransactionHandler{
-		creator:    creator,
-		signer:     signer,
+		builder:     NewTransactionBuilder(client, keyManager),
 		broadcaster: broadcaster,
 	}
 }
 
 func (th *TransactionHandler) HandleTransaction(userId string, conf []config.NetworkConfiguration, to common.Address, value int64, data []byte) {
-	unsignedTx, err := th.creator.CreateTransaction(to, value, data)
+	txHash, err := th.builder.SendRawTransaction(userId, conf, to, big.NewInt(value), data)
 	if err != nil {
-		log.Fatalf("Error creating transaction: %v", err)
+		log.Fatalf("Error creating and signing transaction: %v", err)
 		return
 	}
 
-	signedTx, err := th.signer.SignTransaction(userId, conf, unsignedTx)
-	if err != nil {
-		log.Fatalf("Error signing transaction: %v", err)
-		return
-	}
-
-	th.broadcaster.BroadcastTransaction(signedTx)
+	log.Printf("Transaction successfully sent. Hash: %s", txHash)
 }
