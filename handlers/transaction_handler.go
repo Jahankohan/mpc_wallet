13c58e5a345743ac2e9e9612d8785f04cdb729be
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-06-19 00:42:45 +0300

    handler changed to adopt with gin and also reflect the major changes

--- handlers/transaction_handler.go
+++ handlers/transaction_handler.go
@@ -1,53 +1,47 @@
 package handlers
 
 import (
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
+	"log"
+	"math/big"
 	"net/http"
-	"strconv"
 
 	"github.com/Jahankohan/mpc_wallet/transaction"
 	"github.com/Jahankohan/mpc_wallet/utils"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/gin-gonic/gin"
 )
 
 type TransactionHandler struct {
-	transactionCreator  transaction.TransactionCreator
-	transactionSigner   transaction.TransactionSigner
-	transactionBroadcaster transaction.TransactionBroadcaster
+    transactionBuilder *transaction.TransactionBuilder
 }
 
 type TransactionRequest struct {
-	UserId 	string	`json:"userId"`
-	To     	string	`json:"to"`
-	Value  	string	`json:"value"`
-	Data   	string	`json:"data"` // Additional field for transaction data
-	Network	string	`json:"network"` // Network type (e.g., "mainnet" or "testnet")
+	UserId  string `json:"userId"`
+	To      string `json:"to"`
+	Value   string `json:"value"`
+	Data    string `json:"data"` // Additional field for transaction data
+	Network string `json:"network"`
 }
 
-func NewTransactionHandler(tc transaction.TransactionCreator, ts transaction.TransactionSigner, tb transaction.TransactionBroadcaster) *TransactionHandler {
-	return &TransactionHandler{
-		transactionCreator:  tc,
-		transactionSigner:   ts,
-		transactionBroadcaster: tb,
-	}
+func NewTransactionHandler(tb *transaction.TransactionBuilder) *TransactionHandler {
+    return &TransactionHandler{
+        transactionBuilder: tb,
+    }
 }
 
-func (th *TransactionHandler) HandleTransaction(w http.ResponseWriter, r *http.Request) {
+func (th *TransactionHandler) HandleTransaction(c *gin.Context) {
 	var requestData TransactionRequest
-	err := json.NewDecoder(r.Body).Decode(&requestData)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	if err := c.ShouldBindJSON(&requestData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	// Validate network type
 	if requestData.Network != "mainnet" && requestData.Network != "testnet" {
-		http.Error(w, "Invalid network type. Must be 'mainnet' or 'testnet'", http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid network type. Must be 'mainnet' or 'testnet'"})
 		return
 	}
+
 	is_testnet := true
 	if requestData.Network == "mainnet" {
 		is_testnet = false
@@ -59,48 +53,25 @@ func (th *TransactionHandler) HandleTransaction(w http.ResponseWriter, r *http.R
 
 	// Convert strings to appropriate types
 	toAddress := common.HexToAddress(requestData.To)
-	value, err := strconv.ParseInt(requestData.Value, 10, 64)
-	if err != nil {
-		http.Error(w, "Invalid value format", http.StatusBadRequest)
+	value, success := new(big.Int).SetString(requestData.Value, 10)
+	if !success {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid value format"})
 		return
 	}
 	txData := []byte(requestData.Data)
 
-	// Create transaction
-	unsignedTx, err := th.transactionCreator.CreateTransaction(toAddress, value, txData)
+	// Send raw transaction
+	txHash, err := th.transactionBuilder.SendRawTransaction(requestData.UserId, networkConfigurations, toAddress, value, txData)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to create transaction: %v", err), http.StatusInternalServerError)
-		return
+		log.Fatalf("Error sending transaction: %v", err)
 	}
-
-	// Sign transaction
-	signedTx, err := th.transactionSigner.SignTransaction(requestData.UserId, networkConfigurations, unsignedTx)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to sign transaction: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	// Encode transaction to raw bytes and convert to hex string
-	rawTxBytes, err := rlp.EncodeToBytes(signedTx)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to encode transaction: %v", err), http.StatusInternalServerError)
-		return
-	}
-	rawTxHex := hex.EncodeToString(rawTxBytes)
 
 	// Response
 	response := struct {
-		SignedTransaction string `json:"signedTransaction"`
+		TransactionHash string `json:"transactionHash"`
 	}{
-		SignedTransaction: rawTxHex,
+		TransactionHash: txHash,
 	}
 
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonResponse)
+	c.JSON(http.StatusOK, response)
 }
