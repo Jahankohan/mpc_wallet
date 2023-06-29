a98afbf8cd370eaf9fcebda15e6f4102cadaf981
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-06-30 00:34:06 +0300

    Transaction handler now can separate the write/read transactions and send each one to designated endpoint

--- transaction/transaction_handler.go
+++ transaction/transaction_handler.go
@@ -1,11 +1,12 @@
 package transaction
 
 import (
-	"log"
-	"math/big"
+	"fmt"
+	"strings"
 
 	"github.com/Jahankohan/mpc_wallet/config"
 	"github.com/Jahankohan/mpc_wallet/key_manager"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -22,12 +23,24 @@ func NewTransactionHandler(client *ethclient.Client, keyManager *key_manager.Key
 	}
 }
 
-func (th *TransactionHandler) HandleTransaction(userId string, conf []config.NetworkConfiguration, to common.Address, value int64, data []byte) {
-	txHash, err := th.builder.SendRawTransaction(userId, conf, to, big.NewInt(value), data)
+func (th *TransactionHandler) HandleTransaction(userId string, conf []config.NetworkConfiguration, abiStr string, contractAddress common.Address, functionName string, args ...interface{}) (interface{}, error) {
+	// Parse the ABI
+	parsedABI, err := abi.JSON(strings.NewReader(abiStr))
 	if err != nil {
-		log.Fatalf("Error creating and signing transaction: %v", err)
-		return
+		return nil, fmt.Errorf("failed to parse contract ABI: %w", err)
 	}
 
-	log.Printf("Transaction successfully sent. Hash: %s", txHash)
+	// Check if the function is constant
+	method, exist := parsedABI.Methods[functionName]
+	if !exist {
+		return nil, fmt.Errorf("function %s does not exist in contract ABI", functionName)
+	}
+
+
+	// If the function is constant, call ReadContract. Otherwise, call WriteContract.
+	if (method.StateMutability == "pure" || method.StateMutability == "view")  {
+		return th.builder.ReadContract(abiStr, contractAddress, functionName, args...)
+	} else {
+		return th.builder.WriteContract(userId, conf, abiStr, contractAddress, functionName, args...)
+	}
 }
