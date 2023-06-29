b3d6b5c4d04e3c045bf4cd5870f1a49da9368d2d
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-06-30 00:33:20 +0300

    Transaction builder has clear idea about the type of the method, wether it's a call or a transact, handled by implementing writing and reading methods

--- transaction/transaction_builder.go
+++ transaction/transaction_builder.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"strings"
 
 	"github.com/Jahankohan/mpc_wallet/config"
 	"github.com/Jahankohan/mpc_wallet/key_manager"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -26,7 +29,49 @@ func NewTransactionBuilder(client *ethclient.Client, keyManager *key_manager.Key
 	}
 }
 
-func (tb *TransactionBuilder) SendRawTransaction(userID string, confs []config.NetworkConfiguration, toAddress common.Address, value *big.Int, data []byte) (string, error) {
+func (tb *TransactionBuilder) ReadContract(abiStr string, contractAddress common.Address, functionName string, args ...interface{}) (string, error) {
+	// Parse the ABI
+	parsedABI, err := abi.JSON(strings.NewReader(abiStr))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse contract ABI: %w", err)
+	}
+
+	// Pack the function call
+	data, err := parsedABI.Pack(functionName, args...)
+	if err != nil {
+		return "", fmt.Errorf("failed to pack data for function %q: %w", functionName, err)
+	}
+
+	// Define the call message
+	msg := ethereum.CallMsg{
+		To:   &contractAddress,
+		Data: data,
+	}
+
+	// Call the contract
+	result, err := tb.client.CallContract(context.Background(), msg, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to call contract: %w", err)
+	}
+
+	// The result is the raw bytes returned by the contract function. 
+	// You may want to further process this result depending on your application's needs.
+	return string(result), nil
+}
+
+func (tb *TransactionBuilder) WriteContract(userID string, confs []config.NetworkConfiguration, abiStr string, contractAddress common.Address, functionName string, args ...interface{}) (string, error) {
+	// Parse the ABI
+	parsedABI, err := abi.JSON(strings.NewReader(abiStr))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse contract ABI: %w", err)
+	}
+
+	// Pack the function call
+	data, err := parsedABI.Pack(functionName, args...)
+	if err != nil {
+		return "", fmt.Errorf("failed to pack data for function %q: %w", functionName, err)
+	}
+
 	// Retrieve shares and reconstruct the private key using KeyManager
 	shares, err := tb.keyManager.RetrieveAllShares(confs, userID)
 	if err != nil {
@@ -36,6 +81,8 @@ func (tb *TransactionBuilder) SendRawTransaction(userID string, confs []config.N
 	if err != nil {
 		return "", fmt.Errorf("failed to reconstruct private key: %v", err)
 	}
+	log.Println("Reconstructed Key:", privateKey)
+	log.Println("Public Key:", privateKey.PublicKey)
 
 	// Get the nonce for the account
 	nonce, err := tb.client.PendingNonceAt(context.Background(), crypto.PubkeyToAddress(privateKey.PublicKey))
@@ -49,17 +96,25 @@ func (tb *TransactionBuilder) SendRawTransaction(userID string, confs []config.N
 		return "", err
 	}
 
-	// Create a new transaction
-	tx := types.NewTransaction(nonce, toAddress, value, 52184, gasPrice, data)
-
-	// Sign the transaction
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(43113)), privateKey)
+	// Estimate the gas needed for the transaction
+	msg := ethereum.CallMsg{
+		To:   &contractAddress,
+		Data: data,
+	}
+	gasLimit, err := tb.client.EstimateGas(context.Background(), msg)
 	if err != nil {
 		return "", err
 	}
-	pubKey := privateKey.PublicKey
-	pubKeyHex := crypto.PubkeyToAddress(pubKey)
-	fmt.Println("It's the pub key:", pubKeyHex)
+
+	// Create a new transaction
+	tx := types.NewTransaction(nonce, contractAddress, big.NewInt(0), gasLimit, gasPrice, data)
+
+	// Sign the transaction
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1)), privateKey)
+	if err != nil {
+		return "", err
+	}
+
 	// Log and return the transaction hash
 	txHash := signedTx.Hash().Hex()
 	log.Printf("Transaction sent: %s", txHash)
