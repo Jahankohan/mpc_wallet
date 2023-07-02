e6b3e53461fcba3f59a389fc57247e11d6ff177f
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-07-02 03:35:48 +0300

    Test for regular transaction added, working on meta transaction tests

--- main.go
+++ main.go
@@ -1,62 +1,70 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"strings"
+	"math/big"
 
 	"github.com/Jahankohan/mpc_wallet/key_manager"
 	"github.com/Jahankohan/mpc_wallet/transaction"
 	"github.com/Jahankohan/mpc_wallet/utils"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func main() {
-	client, err := ethclient.Dial("https://api.avax-test.network/ext/bc/C/rpc")
+func test_regular_transaction() {
+	userID := "user12344567"
+
+	// Load configurations
+	configuration := utils.LoadConfig()
+	confs := utils.GetNetworkConfigurations(configuration, true)
+	km := &key_manager.KeyManager{}
+
+	// Replace with your contract's ABI JSON string
+	contract_abi := `[ { "inputs": [ { "internalType": "string", "name": "uri", "type": "string" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": false, "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "ApprovalForAll", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "previousAdminRole", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "newAdminRole", "type": "bytes32" } ], "name": "RoleAdminChanged", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" } ], "name": "RoleGranted", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" } ], "name": "RoleRevoked", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256[]", "name": "ids", "type": "uint256[]" }, { "indexed": false, "internalType": "uint256[]", "name": "values", "type": "uint256[]" } ], "name": "TransferBatch", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "id", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "TransferSingle", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "string", "name": "value", "type": "string" }, { "indexed": true, "internalType": "uint256", "name": "id", "type": "uint256" } ], "name": "URI", "type": "event" }, { "inputs": [], "name": "DEFAULT_ADMIN_ROLE", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "MINTER_ROLE", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" }, { "internalType": "uint256", "name": "id", "type": "uint256" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address[]", "name": "accounts", "type": "address[]" }, { "internalType": "uint256[]", "name": "ids", "type": "uint256[]" } ], "name": "balanceOfBatch", "outputs": [ { "internalType": "uint256[]", "name": "", "type": "uint256[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "role", "type": "bytes32" } ], "name": "getRoleAdmin", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" } ], "name": "grantRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" } ], "name": "hasRole", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" }, { "internalType": "address", "name": "operator", "type": "address" } ], "name": "isApprovedForAll", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" } ], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "nextTokenId", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" } ], "name": "renounceRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" } ], "name": "revokeRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256[]", "name": "ids", "type": "uint256[]" }, { "internalType": "uint256[]", "name": "amounts", "type": "uint256[]" }, { "internalType": "bytes", "name": "data", "type": "bytes" } ], "name": "safeBatchTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "operator", "type": "address" }, { "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "setApprovalForAll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "_newCounter", "type": "uint256" } ], "name": "setCounter", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "simpleCounter", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" } ], "name": "supportsInterface", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "uri", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" } ]`
+
+	// Initialize Ethereum client
+	networkConfig, err := utils.GetSpecificNetworkConfiguration(configuration, "testnet", "fuji")
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := ethclient.Dial(networkConfig.Network)
+	if err != nil {
+		log.Fatalf("Failed to connect to Ethereum network: %s", err)
+	}
+
+	// Initialize Transaction Broadcaster and Handler
+	broadcaster := transaction.NewTransactionBroadcaster(client)
+	metaProcessor, err := transaction.NewMetaTransactionProcessor(broadcaster, networkConfig.Network)
+	
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Initialize the TransactionHandler with the broadcaster and metaProcessor
+	handler := transaction.NewTransactionHandler(broadcaster, *metaProcessor, km, networkConfig)
+
+	toAddress := common.HexToAddress("0xEAc541684a956Eb9Cb7885Ab9A183f36a90411A2")
+
+	// Handle a regular transaction
+	txHash, err := handler.HandleTransaction(networkConfig, userID, confs, contract_abi, toAddress, "setCounter", big.NewInt(1345823))
+	if err != nil {
+		log.Fatalf("Failed to handle transaction: %s", err)
+	}
+
+	fmt.Printf("Transaction sent with hash: %s\n", txHash)
+}
+
+func main() {
 	userID := "user12344567"
+
+	// Load configurations
 	configuration := utils.LoadConfig()
-	confs := utils.GetNetworkConfigurations(configuration, true)
-
-	// fmt.Println("Network Configurations:", confs)
-
-	// Initialize the KeyManager
+	// confs := utils.GetNetworkConfigurations(configuration, true)
 	km := &key_manager.KeyManager{}
 
-	// pk := km.CreatePrivateKey()
-	// fmt.Println("Private Key:", pk)
-	// shares, err := km.SplitToShares(pk, 2, 3)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// strShares := make([]string, len(shares))
-
-	// for i, bytes := range shares {
-	// 	strShares[i] = string(bytes)
-	// }
-	// fmt.Println("Created Shares:", strShares)
-
-	// km.StoreSharesToTheBlockchain(userID, strShares, confs)
-
-	// Restore the key
-	// new_shares, err := km.RetrieveAllShares(confs, userID)
-	// if err != nil {
-	// 	fmt.Println("Can't Retrieve the shares")
-	// }
-	// fmt.Println("Retrieved Shares:", new_shares)
-
-	// key, err := km.ReconstructPrivateKey(new_shares)
-	// if err != nil {
-	// 	fmt.Println("Cant reconstruct the key.")
-	// }
-	// fmt.Println("Reconstructed Key:", key)
-
-	// Initialize the TransactionBuilder with client and KeyManager
-
+	// Replace with your contract's ABI JSON string
 	contract_abi := `[
 		{
 			"inputs": [
@@ -64,6 +72,11 @@ func main() {
 					"internalType": "string",
 					"name": "uri",
 					"type": "string"
+				},
+				{
+					"internalType": "address",
+					"name": "_forwarder",
+					"type": "address"
 				}
 			],
 			"stateMutability": "nonpayable",
@@ -331,6 +344,47 @@ func main() {
 					"internalType": "uint256[]",
 					"name": "",
 					"type": "uint256[]"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "uint256",
+					"name": "_newCounter",
+					"type": "uint256"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "sigR",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "sigS",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "uint8",
+					"name": "sigV",
+					"type": "uint8"
+				}
+			],
+			"name": "executeSetCounterMetaTx",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [],
+			"name": "forwarder",
+			"outputs": [
+				{
+					"internalType": "contract IForwarder",
+					"name": "",
+					"type": "address"
 				}
 			],
 			"stateMutability": "view",
@@ -647,22 +701,127 @@ func main() {
 			"type": "function"
 		}
 	]`
-	parsedABI, err := abi.JSON(strings.NewReader(contract_abi))
-	if err != nil {
-		log.Fatalf("Failed to parse contract ABI: %v", err)
-	}
-	data, err := parsedABI.Pack("simpleCounter")
-	if err != nil {
-		log.Fatalf("Failed to parse method: %v", err)
-	}
-	broadcaster := transaction.NewTransactionBroadcaster(client)
-	handler := transaction.NewTransactionHandler(client, km, broadcaster)
+	forwarder_abi := `[
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": false,
+					"internalType": "address",
+					"name": "userAddress",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"internalType": "address",
+					"name": "relayerAddress",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"internalType": "bytes",
+					"name": "functionSignature",
+					"type": "bytes"
+				}
+			],
+			"name": "MetaTransactionExecuted",
+			"type": "event"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "userAddress",
+					"type": "address"
+				},
+				{
+					"internalType": "bytes",
+					"name": "functionSignature",
+					"type": "bytes"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "sigR",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "bytes32",
+					"name": "sigS",
+					"type": "bytes32"
+				},
+				{
+					"internalType": "uint8",
+					"name": "sigV",
+					"type": "uint8"
+				}
+			],
+			"name": "executeMetaTransaction",
+			"outputs": [],
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"inputs": [
+				{
+					"internalType": "address",
+					"name": "user",
+					"type": "address"
+				}
+			],
+			"name": "getNonce",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"stateMutability": "view",
+			"type": "function"
+		}
+	]`
 	
-	
-	
-	toAddress := common.HexToAddress("0xEAc541684a956Eb9Cb7885Ab9A183f36a90411A2")
-	value := int64(0)
-	
+	forwarder_function_name := "executeMetaTransaction"
 
-	handler.HandleTransaction(userID, confs, toAddress, value, data)
+	// Initialize Ethereum client
+	networkConfig, err := utils.GetSpecificNetworkConfiguration(configuration, "testnet", "fuji")
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := ethclient.Dial(networkConfig.Network)
+	if err != nil {
+		log.Fatalf("Failed to connect to Ethereum network: %s", err)
+	}
+
+	// Initialize Transaction Broadcaster and Meta Transaction Processor
+	broadcaster := transaction.NewTransactionBroadcaster(client)
+	metaProcessor, err := transaction.NewMetaTransactionProcessor(broadcaster, networkConfig.Network)
+
+	// Initialize the TransactionHandler with the broadcaster and metaProcessor
+	handler := transaction.NewTransactionHandler(broadcaster, *metaProcessor, km, networkConfig)
+
+	contractAddress := common.HexToAddress("0x8fF38909B55dC039D0bc8D2d0c25bFF526D29E5F")
+	forwarder_address := common.HexToAddress("0x74e8da81ed6D4d12d68542351A9c31F7613e264e")
+	
+	// Handle a meta transaction
+	txHash, err := handler.HandleMetaTransaction(
+		context.Background(),
+		networkConfig,
+		km,
+		userID,
+		forwarder_abi,
+		forwarder_address,
+		forwarder_function_name,
+		big.NewInt(0), // nonce
+		contractAddress,
+		contract_abi,
+		"setCounter",
+		
+		big.NewInt(13423), // argument to setCounter
+	)
+	if err != nil {
+		log.Fatalf("Failed to handle meta transaction: %s", err)
+	}
+
+	fmt.Printf("Meta Transaction sent with hash: %s\n", txHash)
 }
