72819977393072e0676bcde630d1db05d6ab3c65
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-07-07 02:12:48 +0300

    meta transaction refactored, it's now working properly, but need a more in depth refactoring, currently the siging and relaying mechanism is in the same package/file

--- transaction/meta_transaction.go
+++ transaction/meta_transaction.go
@@ -10,14 +10,15 @@ import (
 	"os"
 	"strings"
 
+	"github.com/Jahankohan/mpc_wallet/Forwarder"
 	"github.com/Jahankohan/mpc_wallet/config"
 	"github.com/Jahankohan/mpc_wallet/key_manager"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"golang.org/x/crypto/sha3"
 )
 
 type MetaTransactionProcessor struct {
@@ -38,36 +39,150 @@ func NewMetaTransactionProcessor(broadcaster TransactionBroadcaster, rpcURL stri
 	}, nil
 }
 
-func hexStringToBytes32(s string) [32]byte {
-    bytes, err := hex.DecodeString(s)
-    if err != nil {
-        log.Fatalf("Failed to decode hex string: %v", err)
-    }
-    
-    var bytes32 [32]byte
-    copy(bytes32[:], bytes)
-    return bytes32
+func (mtp *MetaTransactionProcessor) padTo32Bytes(input []byte) []byte {
+	padded := make([]byte, 32)
+	copy(padded[32-len(input):], input)
+	return padded
 }
 
-func (mtp *MetaTransactionProcessor) ProcessMetaTransaction(ctx context.Context, networkConfig config.NetworkConfiguration, keyManager *key_manager.KeyManager, userId string, confs []config.NetworkConfiguration, forwarderAbiStr string, forwarderAddress common.Address, forwarderFunctionName string, nonce *big.Int, contractAddress common.Address, contractAbiStr string, contractFunctionName string, args ...interface{}) (string, error) {
-
-	// Parse the Forwarder Contract ABI
-	forwarderABI, err := abi.JSON(strings.NewReader(forwarderAbiStr))
+func (mtp *MetaTransactionProcessor) getNonce(userAddress common.Address, forwarder *Forwarder.Forwarder) (*big.Int, error){
+	nonce, err := forwarder.GetNonce(&bind.CallOpts{}, userAddress)
 	if err != nil {
-		return "", fmt.Errorf("failed to parse forwarder contract ABI: %w", err)
+		fmt.Printf("Failed to call getNonce: %v\n", err)
+		return big.NewInt(-1), err
+	}
+	return nonce, nil
+}
+
+func (mtp *MetaTransactionProcessor) convertNonceToBytes(nonce string) ([]byte, error) {
+	nonceInBytes := mtp.padTo32Bytes([]byte(nonce))
+	fmt.Println("nonce:", hex.EncodeToString(nonceInBytes[:]))
+	return nonceInBytes, nil
+}
+
+func (mtp *MetaTransactionProcessor) calculateDigest(domainSeparator []byte, 
+	messageTypeHash []byte, 
+	functionName []byte, 
+	paddedUserNonce string,
+	userAddress string) []byte {
+
+	functionSignaure := crypto.Keccak256(functionName)
+	firstLayer := hex.EncodeToString(messageTypeHash[:]) + paddedUserNonce + userAddress + hex.EncodeToString(functionSignaure[:])
+	bytes, err := hex.DecodeString(firstLayer)
+	if err != nil {
+		fmt.Println("Error decoding hex string:", err)
+	}
+	firstLayerDigest := crypto.Keccak256(bytes)
+	fmt.Println("Calculate Digest ---  Type Hash:", hex.EncodeToString(messageTypeHash[:]))
+	fmt.Println("Calculate Digest ---  User Nonce:", paddedUserNonce)
+	fmt.Println("Calculate Digest ---  User Address:", userAddress)
+	fmt.Println("Calculate Digest ---  Function Signature:", hex.EncodeToString(functionSignaure[:]))
+	fmt.Println("Calculate Digest ---  First Layer ABI:", firstLayer)
+	fmt.Println("Calculate Digest ---  First Layer Keccak:", hex.EncodeToString(firstLayerDigest[:]))
+
+	prefix := []byte{0x19, 0x01}
+    encodedPackedData := append(prefix, domainSeparator[:]...)
+    encodedPackedData = append(encodedPackedData, firstLayerDigest...)
+	fmt.Println("Calculate Digest ---  Second Layer", hex.EncodeToString(encodedPackedData[:]))
+
+	digest := crypto.Keccak256(encodedPackedData)
+	fmt.Println("Calculate Digest ---  Digest", hex.EncodeToString(digest[:]))
+
+	return digest
+
+} 
+
+func (mtp *MetaTransactionProcessor) calculateDomainSeparator(EIP712_DOMAIN_TYPEHASH string, nameHash string, 
+								versionHash string, paddedChainId string, contractAddress string) []byte{
+
+	fmt.Println("Forwarder Address:", contractAddress)
+	fmt.Println("Padded ChainId:", (paddedChainId))
+
+	// Concatenate all bytes
+	encoded := EIP712_DOMAIN_TYPEHASH + nameHash + versionHash + paddedChainId + contractAddress
+
+	// Display the concatenated bytes
+	fmt.Println("Concatenated Bytes:", encoded)
+
+	// Get bytes from the hexadecimal string
+	bytes, err := hex.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("Error decoding hex string:", err)
 	}
 
-	// Parse the Target Contract ABI
-	contractABI, err := abi.JSON(strings.NewReader(contractAbiStr))
+	// Now, you can hash the concatenated bytes with Keccak256
+	domainSeparator := crypto.Keccak256(bytes) // Assuming you have a Keccak256 function defined elsewhere
+
+	fmt.Printf("DOMAIN SEPARATOR: %x\n", domainSeparator)
+	return domainSeparator
+}
+
+func (mtp *MetaTransactionProcessor) SignMetaTransaction(
+	privateKey *ecdsa.PrivateKey, 
+	userAddress common.Address, 
+	functionSignature []byte, 
+	chainID *big.Int, 
+	forwarder common.Address, 
+	nonce *big.Int,
+	forwarderInstance *Forwarder.Forwarder,
+	) ([]byte, error) {
+    // Define EIP-712 types
+    EIP712Domain := "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+    META_TRANSACTION_TYPE := "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
+	name := "MyApp"
+	version := "1"
+	nameHash := crypto.Keccak256([]byte(name))
+	versionHash := crypto.Keccak256([]byte(version))
+
+    // Domain separator and type hashes
+    domainSeparatorTypeHash := crypto.Keccak256([]byte(EIP712Domain))
+    metaTransactionTypeHash := crypto.Keccak256([]byte(META_TRANSACTION_TYPE))
+
+
+	paddedChainId := mtp.padTo32Bytes(chainID.Bytes())
+	paddedAddress := mtp.padTo32Bytes(forwarder.Bytes())
+	domainSeparator := mtp.calculateDomainSeparator(hex.EncodeToString(domainSeparatorTypeHash[:]), 
+												hex.EncodeToString(nameHash[:]), 
+												hex.EncodeToString(versionHash[:]), 
+												hex.EncodeToString(paddedChainId[:]), 
+												hex.EncodeToString(paddedAddress[:]))
+	
+	userNonce, err := mtp.getNonce(userAddress, forwarderInstance)
 	if err != nil {
-		return "", fmt.Errorf("failed to parse target contract ABI: %w", err)
+		log.Fatal(err)
+	}
+	paddedUserNonce := mtp.padTo32Bytes(userNonce.Bytes())
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Pack the data for the function we want to call in the target contract
-	functionData, err := contractABI.Pack(contractFunctionName, args...)
+	paddedUserAddress := mtp.padTo32Bytes(userAddress.Bytes())
+												
+	digest := mtp.calculateDigest(domainSeparator, metaTransactionTypeHash, functionSignature, 
+								hex.EncodeToString(paddedUserNonce), hex.EncodeToString(paddedUserAddress[:]))
+
+
+    // Sign the digest
+    signature, err := crypto.Sign(digest, privateKey)
     if err != nil {
-        return "", fmt.Errorf("failed to pack data for target contract: %w", err)
+        return nil, err
     }
+
+    return signature, nil
+}
+
+
+func (mtp *MetaTransactionProcessor) ProcessMetaTransaction(ctx context.Context, 
+	networkConfig config.NetworkConfiguration, keyManager *key_manager.KeyManager, userId string, 
+	confs []config.NetworkConfiguration, forwarderAbiStr string, forwarderAddress common.Address, 
+	forwarderFunctionName string, nonce *big.Int, contractAddress common.Address, contractAbiStr string, 
+	contractFunctionName string, args ...interface{}) (string, error) {
+	
+	// Get relayer's private key from environment variable
+	privateKey, err := crypto.HexToECDSA(os.Getenv("RELAYER_PRIVATE_KEY"))
+	if err != nil {
+		return "", fmt.Errorf("failed to load relayer's private key: %w", err)
+	}
 
 	// Retrieve shares and reconstruct the private key using KeyManager
 	shares, err := keyManager.RetrieveAllShares(confs, userId)
@@ -84,153 +199,72 @@ func (mtp *MetaTransactionProcessor) ProcessMetaTransaction(ctx context.Context,
 	}
 	userAddress := crypto.PubkeyToAddress(rawUserPrivateKey.PublicKey)
 
-	// Retrieve nonce from the forwarder contract
-	forwarderNonce, err := mtp.getNonce(ctx, userAddress, forwarderAddress, forwarderABI)
-	if err != nil {
-    	return "", fmt.Errorf("failed to retrieve nonce: %v", err)
-	}
 
-	fmt.Println("Forwarder Nonce:", forwarderNonce)
-
-	dataToSign := crypto.Keccak256(
-		userAddress.Bytes(),
-		forwarderAddress.Bytes(),
-		forwarderNonce.Bytes(),
-		functionData,
-	)
-
-	prefix := []byte("\x19Ethereum Signed Message:\n32")
-	prefixedData := append(prefix, dataToSign...)
-	prefixedHash := crypto.Keccak256(prefixedData)
-
-	userPrivateKeyBytes, err := hex.DecodeString(userPrivateKey)
+	// Load the Forwarder contract
+	forwarderInstance, err := Forwarder.NewForwarder(forwarderAddress, mtp.ethClient)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Convert bytes to ecdsa.PrivateKey
-	privateKeyECDSA, err := crypto.ToECDSA(userPrivateKeyBytes)
+	chainId := new(big.Int)
+
+	// Convert the string to big.Int
+	_, ok := chainId.SetString(networkConfig.ChainId, 10) // 10 is the base (decimal in this case)
+	if !ok {
+		fmt.Println("Error in conversion")
+	}
+
+	// Create an authorized transactor
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainId)
+	if err != nil {
+		fmt.Println("Error Binding the auth, %w", err)
+	}
+	auth.Value = big.NewInt(0)      // in wei
+	auth.GasLimit = uint64(3000000) // in units
+	gasPrice, err := mtp.ethClient.SuggestGasPrice(context.Background())
+	if err != nil {
+    	log.Fatal(err)
+	}
+	auth.GasPrice = gasPrice
+
+	// Parse the Target Contract ABI
+	contractABI, err := abi.JSON(strings.NewReader(contractAbiStr))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse target contract ABI: %w", err)
+	}
+
+	functionSignature, err := contractABI.Pack(contractFunctionName, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// sign data with user's private key
-	signature, err := crypto.Sign(prefixedHash, privateKeyECDSA)
+	
+	// Sign the meta transaction
+	signature, err := mtp.SignMetaTransaction(rawUserPrivateKey, userAddress, 
+		functionSignature, chainId, forwarderAddress, big.NewInt(0), forwarderInstance)
 	if err != nil {
-        return "", fmt.Errorf("failed to sign data: %w", err)
+		log.Fatal(err)
+	}
+
+	// Extract r, s, v from the signature
+	r := [32]byte{}
+	s := [32]byte{}
+	copy(r[:], signature[:32])
+	copy(s[:], signature[32:64])
+	v := uint8(signature[64]) + 27
+
+	// Pack the data for the function we want to call in the target contract
+	functionData, err := contractABI.Pack(contractFunctionName, args...)
+    if err != nil {
+        return "", fmt.Errorf("failed to pack data for target contract: %w", err)
     }
 
-	chainIdInt := new(big.Int)
-    _, success := chainIdInt.SetString(networkConfig.ChainId, 10)
-    if !success {
-        log.Fatal("Error Converting ChainId")
-    }
-	fmt.Println("chainIdInt:", chainIdInt)
-
-    sigR := signature[:32]
-    sigS := signature[32:64]
-    sigV := signature[64]
-
-	var sigRFixed [32]byte
-	var sigSFixed [32]byte
-	copy(sigRFixed[:], sigR)
-	copy(sigSFixed[:], sigS)
-
-	// Convert byte to uint8
-	sigVUint8 := uint8(sigV)
-	fmt.Println(sigVUint8)
-	// if sigVUint8 < 27 {
-	// 	sigVUint8 += byte(chainIdInt.Uint64()*2 + 35)
-	// }
-	// fmt.Println("New SigV value:", sigVUint8)
-
-	// Calculate the new v value based on the chain ID
-	chainID := big.NewInt(43113) // Your chainID here
-	v := big.NewInt(int64(sigV)) // Your recovery id (0 or 1) as big.Int
-
-	// Calculate sigV according to EIP-155
-	v.Add(v, chainID.Mul(chainID, big.NewInt(2)))
-	v.Add(v, big.NewInt(35))
-
-	// Convert to uint8
-	sigVUint8 = byte(v.Uint64())
-	fmt.Println("New Sigv:", sigVUint8)
-	fmt.Println("SigR:", sigRFixed)
-	fmt.Println("SigS:", sigSFixed)
-	fmt.Println("function:", functionData)
-	fmt.Println("target:", forwarderAddress)
-	fmt.Println("user address:", userAddress)
-
-	Sv, sR, sS, err := CreateAndSignMetaTransaction(privateKeyECDSA, userAddress, 
-		forwarderAddress, forwarderNonce, functionData, chainIdInt)
+	tx, err := forwarderInstance.ExecuteMetaTransaction(auth, userAddress, contractAddress, functionData, r, s, v)
 	if err != nil {
-		fmt.Println("Unable to create and sign.")
+		log.Fatal(err)
 	}
-	fmt.Println("New Calculated Vares")
-	fmt.Println("New Sigv:", Sv)
-	fmt.Println("SigR:", sR)
-	fmt.Println("SigS:", sS)
+	fmt.Printf("Meta Transaction sent: %s\n", tx.Hash().Hex())
 
-	verifyResult, err := VerifyMetaTransaction(userAddress, forwarderAddress, forwarderNonce, 
-	functionData, 
-	Sv,
-	sR, 
-	sS)
-	if err != nil {
-		return "", fmt.Errorf("failed to verify the hash: %w", err)
-	}
-	fmt.Println("Hash Verified?", verifyResult)
-
-	// Pack the data for the function in the forwarder contract (processMetaTransaction) along with the required meta-transaction parameters
-	data, err := forwarderABI.Pack(
-        forwarderFunctionName, 
-        userAddress, 
-        functionData,
-		sigRFixed,
-		sigSFixed,
-		sigVUint8,
-    )
-	if err != nil {
-		return "", fmt.Errorf("failed to pack data for forwarder contract: %w", err)
-	}
-
-	// Get relayer's private key from environment variable
-	privateKey, err := crypto.HexToECDSA(os.Getenv("RELAYER_PRIVATE_KEY"))
-	if err != nil {
-		return "", fmt.Errorf("failed to load relayer's private key: %w", err)
-	}
-
-	// Estimate gas limit
-	gasLimit, err := mtp.estimateGasLimit(ctx, forwarderAddress, data)
-	if err != nil {
-		return "", fmt.Errorf("failed to estimate gas limit: %w", err)
-	}
-
-	// Get gas price
-	gasPrice, err := mtp.estimateGasPrice(ctx)
-	if err != nil {
-		return "", fmt.Errorf("failed to estimate gas price: %w", err)
-	}
-
-    fromAddress := crypto.PubkeyToAddress(privateKey.PublicKey)
-   
-
-	// Sign and broadcast the meta-transaction via the forwarder contract
-	txHash, err := mtp.broadcaster.BroadcastMetaTransaction(
-        ctx, 
-        fromAddress,
-        forwarderAddress,
-        big.NewInt(0), // Value is 0 for meta transactions
-        data,
-        gasLimit,
-        gasPrice,
-        chainIdInt,
-    )
-	if err != nil {
-		return "", fmt.Errorf("failed to broadcast meta-transaction: %w", err)
-	}
-
-	// Return the transaction hash
-	return txHash, nil
+	return tx.Hash().Hex(), nil
 }
 
 func (mtp *MetaTransactionProcessor) estimateGasLimit(ctx context.Context, toAddress common.Address, data []byte) (uint64, error) {
@@ -256,100 +290,3 @@ func (mtp *MetaTransactionProcessor) estimateGasPrice(ctx context.Context) (*big
 
 	return gasPrice, nil
 }
-
-func GetFunctionSignature(functionText string) []byte {
-	hasher := sha3.NewLegacyKeccak256()
-	hasher.Write([]byte(functionText))
-	return hasher.Sum(nil)[:4]
-}
-
-func CreateAndSignMetaTransaction(userPrivateKey *ecdsa.PrivateKey, userAddress common.Address, targetContract common.Address, nonce *big.Int, functionSignature []byte, chainID *big.Int) (uint8, [32]byte, [32]byte, error) {
-
-	// Hash the data
-	hash := crypto.Keccak256Hash(
-		userAddress.Bytes(),
-		targetContract.Bytes(),
-		nonce.Bytes(),
-		functionSignature,
-	)
-
-	// Prefix
-	prefix := fmt.Sprintf("\x19Ethereum Signed Message:\n%d", len(hash.Bytes()))
-	prefixedHash := crypto.Keccak256Hash(append([]byte(prefix), hash.Bytes()...))
-
-	// Sign the prefixed hash with the user's private key
-	signature, err := crypto.Sign(prefixedHash.Bytes(), userPrivateKey)
-	if err != nil {
-		return 0, [32]byte{}, [32]byte{}, fmt.Errorf("failed to sign data: %v", err)
-	}
-
-	// Extract V, R, S from the signature
-	sigV := signature[64] // V
-	if chainID.Sign() != 0 {
-		sigV += byte(chainID.Uint64()*2 + 35)
-	}
-	var sigR, sigS [32]byte
-	copy(sigR[:], signature[:32]) // R
-	copy(sigS[:], signature[32:64]) // S
-
-	return sigV, sigR, sigS, nil
-}
-
-func VerifyMetaTransaction(userAddress common.Address, targetContract common.Address, nonce *big.Int, functionSignature []byte, sigV uint8, sigR [32]byte, sigS [32]byte) (bool, error) {
-
-	// Hash the data
-	hash := crypto.Keccak256Hash(
-		userAddress.Bytes(),
-		targetContract.Bytes(),
-		nonce.Bytes(),
-		functionSignature,
-	)
-
-	fmt.Println("Hash Computed:", hash)
-
-	// Prefix
-	prefix := fmt.Sprintf("\x19Ethereum Signed Message:\n%d", len(hash.Bytes()))
-	prefixedHash := crypto.Keccak256Hash(append([]byte(prefix), hash.Bytes()...))
-
-	fmt.Println("Prefixhash computed:", prefixedHash)
-
-	// Reconstruct the sender's public key using the signature
-	sig := append(sigR[:], sigS[:]...)
-	sig = append(sig, sigV-27)
-
-	pubKey, err := crypto.SigToPub(prefixedHash.Bytes(), sig)
-	if err != nil {
-		return false, fmt.Errorf("failed to recover public key: %v", err)
-	}
-	fmt.Println("PubKey Created:", pubKey)
-
-	recoveredAddress := crypto.PubkeyToAddress(*pubKey)
-	fmt.Println("Recovered Address:", recoveredAddress)
-	return recoveredAddress == userAddress, nil
-}
-
-func (mtp *MetaTransactionProcessor) getNonce(ctx context.Context, userAddress common.Address, forwarderAddress common.Address, forwarderABI abi.ABI) (*big.Int, error) {
-    callData, err := forwarderABI.Pack("getNonce", userAddress)
-    if err != nil {
-        return nil, fmt.Errorf("failed to pack data for getNonce: %v", err)
-    }
-
-    msg := ethereum.CallMsg{
-        To:   &forwarderAddress,
-        Data: callData,
-    }
-    output, err := mtp.ethClient.CallContract(ctx, msg, nil)
-    if err != nil {
-        return nil, fmt.Errorf("failed to call getNonce: %v", err)
-    }
-
-    // Unpack result
-    var nonce *big.Int
-    err = forwarderABI.UnpackIntoInterface(&nonce, "getNonce", output)
-    if err != nil {
-        return nil, fmt.Errorf("failed to unpack getNonce output: %v", err)
-    }
-
-    return nonce, nil
-}
-
