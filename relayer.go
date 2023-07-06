8f8f66961173634d000578c65026d3068d2e9de1
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-07-06 22:47:58 +0300

    Hardcoded values removed, now it's more rubost and dynamic

--- relayer.go
+++ relayer.go
@@ -21,22 +21,36 @@ func padTo32Bytes(input []byte) []byte {
 	return padded
 }
 
+func getNonce(userAddress common.Address, forwarder *Forwarder.Forwarder) (*big.Int, error){
+	nonce, err := forwarder.GetNonce(&bind.CallOpts{}, userAddress)
+	if err != nil {
+		fmt.Printf("Failed to call getNonce: %v\n", err)
+		return big.NewInt(-1), err
+	}
+	return nonce, nil
+}
+
+func convertNonceToBytes(nonce string) ([]byte, error) {
+	nonceInBytes := padTo32Bytes([]byte(nonce))
+	fmt.Println("nonce:", hex.EncodeToString(nonceInBytes[:]))
+	return nonceInBytes, nil
+}
+
 func calculateDigest(domainSeparator []byte, 
 	messageTypeHash []byte, 
 	functionName []byte, 
-	userNonce *big.Int) []byte {
-	userAddress := "8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
-	
-	nonce := "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+	paddedUserNonce string,
+	userAddress string) []byte {
+
 	functionSignaure := crypto.Keccak256(functionName)
-	firstLayer := hex.EncodeToString(messageTypeHash[:]) + nonce + userAddress + hex.EncodeToString(functionSignaure[:])
+	firstLayer := hex.EncodeToString(messageTypeHash[:]) + paddedUserNonce + userAddress + hex.EncodeToString(functionSignaure[:])
 	bytes, err := hex.DecodeString(firstLayer)
 	if err != nil {
 		fmt.Println("Error decoding hex string:", err)
 	}
 	firstLayerDigest := crypto.Keccak256(bytes)
 	fmt.Println("Calculate Digest ---  Type Hash:", hex.EncodeToString(messageTypeHash[:]))
-	fmt.Println("Calculate Digest ---  User Nonce:", nonce)
+	fmt.Println("Calculate Digest ---  User Nonce:", paddedUserNonce)
 	fmt.Println("Calculate Digest ---  User Address:", userAddress)
 	fmt.Println("Calculate Digest ---  Function Signature:", hex.EncodeToString(functionSignaure[:]))
 	fmt.Println("Calculate Digest ---  First Layer ABI:", firstLayer)
@@ -54,23 +68,14 @@ func calculateDigest(domainSeparator []byte,
 
 } 
 
-func calculateDomainSeparator() []byte{
-	EIP712_DOMAIN_TYPEHASH := "8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f"
-	nameHash := "1db874843672c48c2b3f8b32abc9aedf3c4132bc5c1094d62797110a8f762d54"
-	versionHash := "c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6"
-	chainID := "0000000000000000000000000000000000000000000000000000000000013881000000000000000000000000"
-	contractAddress := "59EdA1Cde4E71168d119b8089281a88491d7b58b"
+func calculateDomainSeparator(EIP712_DOMAIN_TYPEHASH string, nameHash string, 
+								versionHash string, paddedChainId string, contractAddress string) []byte{
 
-	chainIDNew := big.NewInt(80001)
-	chainIDBytes := chainIDNew.Bytes()
-	chainIDPadded := make([]byte, 32)
-	copy(chainIDPadded[32-len(chainIDBytes):], chainIDBytes)
-	fmt.Println("ChainID Padded:", hex.EncodeToString(chainIDPadded[:]))
-	fmt.Println("ChainID Bytes:", hex.EncodeToString(chainIDBytes[:]))
-
+	fmt.Println("Forwarder Address:", contractAddress)
+	fmt.Println("Padded ChainId:", (paddedChainId))
 
 	// Concatenate all bytes
-	encoded := EIP712_DOMAIN_TYPEHASH + nameHash + versionHash + chainID + contractAddress
+	encoded := EIP712_DOMAIN_TYPEHASH + nameHash + versionHash + paddedChainId + contractAddress
 
 	// Display the concatenated bytes
 	fmt.Println("Concatenated Bytes:", encoded)
@@ -88,43 +93,49 @@ func calculateDomainSeparator() []byte{
 	return domainSeparator
 }
 
-func signMetaTransaction(privateKey *ecdsa.PrivateKey, userAddress common.Address, functionSignature []byte, chainID *big.Int, forwarder common.Address, nonce *big.Int) ([]byte, error) {
+func signMetaTransaction(
+	privateKey *ecdsa.PrivateKey, 
+	userAddress common.Address, 
+	functionSignature []byte, 
+	chainID *big.Int, 
+	forwarder common.Address, 
+	nonce *big.Int,
+	forwarderInstance *Forwarder.Forwarder,
+	) ([]byte, error) {
     // Define EIP-712 types
     EIP712Domain := "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
     META_TRANSACTION_TYPE := "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
-	// name := "MyApp"
-	// version := "1"
-	// nameHash := crypto.Keccak256([]byte(name))
-	// versionHash := crypto.Keccak256([]byte(version))
+	name := "MyApp"
+	version := "1"
+	nameHash := crypto.Keccak256([]byte(name))
+	versionHash := crypto.Keccak256([]byte(version))
 
     // Domain separator and type hashes
     domainSeparatorTypeHash := crypto.Keccak256([]byte(EIP712Domain))
     metaTransactionTypeHash := crypto.Keccak256([]byte(META_TRANSACTION_TYPE))
 
-	fmt.Println("Domain Type Hash:", hex.EncodeToString(domainSeparatorTypeHash[:]))
-	fmt.Println("meta type hash:", hex.EncodeToString(metaTransactionTypeHash[:]))
 
-    // domainData := append(append(append([]byte("MyApp"), []byte("1")...), common.LeftPadBytes(chainID.Bytes(), 32)...), forwarder.Bytes()...)
-
-    // domainSeparator := crypto.Keccak256(
-    //     domainSeparatorTypeHash,
-    //     crypto.Keccak256(domainData),
-    // )
-
-	chainIDBytes := make([]byte, 32)
-	chainID.FillBytes(chainIDBytes)
-	// paddedChainIDBytes := append(make([]byte, 32-len(chainIDBytes)), chainIDBytes...)
-
-	// domainSeparators := crypto.Keccak256(
-	// 	domainSeparatorTypeHash,
-	// 	crypto.Keccak256([]byte("MyApp")),
-	// 	crypto.Keccak256([]byte("1")),
-	// 	chainIDBytes,
-	// 	forwarder.Bytes(),
-	// )
-	domainSeparator := calculateDomainSeparator()
+	paddedChainId := padTo32Bytes(chainID.Bytes())
+	paddedAddress := padTo32Bytes(forwarder.Bytes())
+	domainSeparator := calculateDomainSeparator(hex.EncodeToString(domainSeparatorTypeHash[:]), 
+												hex.EncodeToString(nameHash[:]), 
+												hex.EncodeToString(versionHash[:]), 
+												hex.EncodeToString(paddedChainId[:]), 
+												hex.EncodeToString(paddedAddress[:]))
 	
-	digest := calculateDigest(domainSeparator, metaTransactionTypeHash, functionSignature, big.NewInt(0))
+	userNonce, err := getNonce(userAddress, forwarderInstance)
+	if err != nil {
+		log.Fatal(err)
+	}
+	paddedUserNonce := padTo32Bytes(userNonce.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	paddedUserAddress := padTo32Bytes(userAddress.Bytes())
+												
+	digest := calculateDigest(domainSeparator, metaTransactionTypeHash, functionSignature, 
+								hex.EncodeToString(paddedUserNonce), hex.EncodeToString(paddedUserAddress[:]))
 
 
     // Sign the digest
@@ -134,59 +145,6 @@ func signMetaTransaction(privateKey *ecdsa.PrivateKey, userAddress common.Addres
     }
 
     return signature, nil
-}
-
-
-func verifySignature(
-	userAddress common.Address,
-	functionSignature []byte,
-	chainID *big.Int,
-	forwarder common.Address,
-	nonce *big.Int,
-	sigR, sigS [32]byte,
-	sigV uint8,
-) bool {
-
-	// Define EIP-712 types
-	EIP712Domain := "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-	META_TRANSACTION_TYPE := "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
-
-	// Domain separator and type hashes
-	domainSeparatorTypeHash := crypto.Keccak256([]byte(EIP712Domain))
-	metaTransactionTypeHash := crypto.Keccak256([]byte(META_TRANSACTION_TYPE))
-
-	domainData := []byte("MyApp")
-	domainData = append(domainData, []byte("1")...)
-	domainData = append(domainData, common.LeftPadBytes(chainID.Bytes(), 32)...) // Pad to 32 bytes
-	domainData = append(domainData, forwarder.Bytes()...)
-
-	domainSeparator := crypto.Keccak256(
-		domainSeparatorTypeHash,
-		crypto.Keccak256(domainData),
-	)
-
-	functionSignatureHash := crypto.Keccak256(functionSignature)
-	nonceBytes := common.LeftPadBytes(nonce.Bytes(), 32) // Pad to 32 bytes
-
-	typedDataHash := crypto.Keccak256(
-		metaTransactionTypeHash,
-		crypto.Keccak256(nonceBytes, userAddress.Bytes(), functionSignatureHash),
-	)
-
-	// The final digest that will be used for ecrecover
-	digest := crypto.Keccak256([]byte("\x19\x01"), domainSeparator, typedDataHash)
-
-	pubKey, err := crypto.SigToPub(digest, append(append(sigR[:], sigS[:]...), sigV-27))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	recoveredAddress := crypto.PubkeyToAddress(*pubKey)
-
-	fmt.Println("User Address:", userAddress.Hex())
-	fmt.Println("Recovered Address:", recoveredAddress.Hex())
-
-	return recoveredAddress == userAddress
 }
 
 
@@ -240,7 +198,8 @@ func relayMetaTx(
 	auth.GasPrice = gasPrice
 
 	// Sign the meta transaction
-	signature, err := signMetaTransaction(userPrivateKeyECDSA, userAddress, functionSignature, chainId, forwarderAddress, big.NewInt(0))
+	signature, err := signMetaTransaction(userPrivateKeyECDSA, userAddress, 
+			functionSignature, chainId, forwarderAddress, big.NewInt(0), forwarderInstance)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -260,14 +219,6 @@ func relayMetaTx(
 	// Print function signature in bytes
 	fmt.Println("Function Signature (in bytes):", hex.EncodeToString(functionSignature))
 
-	// res := verifySignature(userAddress, functionSignature, chainId, forwarderAddress, big.NewInt(0), r, s, v)
-	// if res {
-	// 	// Execute the meta transaction
-	
-
-	// } else {
-	// 	fmt.Println("Signnature is not valid.")
-	// }
 	targetContract := common.HexToAddress("0xA99dCd104F08d01fD7A142cB7451c9E64Be8f6e3")
 
 	tx, err := forwarderInstance.ExecuteMetaTransaction(auth, userAddress, targetContract, functionSignature, r, s, v)
