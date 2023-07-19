5e2bce954cf10e47cdaec86d4a85eaa7d435b358
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-07-20 01:59:35 +0300

    CORS middleware added

--- cmd/server.go
+++ cmd/server.go
@@ -9,48 +9,42 @@ import (
 	"github.com/Jahankohan/mpc_wallet/middlewares"
 	"github.com/Jahankohan/mpc_wallet/models"
 	"github.com/Jahankohan/mpc_wallet/utils"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RunServer() {
-	// Load configurations
-	middlewares.LoadPermissions()
-	configuration := utils.LoadConfig()
-	dbConfig := configuration.Database
-	km := key_manager.KeyManager{}
+        // Load configurations
+        middlewares.LoadPermissions()
+        configuration := utils.LoadConfig()
+        dbConfig := configuration.Database
+        km := key_manager.KeyManager{}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-		dbConfig.Host, dbConfig.Username, dbConfig.Password, dbConfig.DBName, dbConfig.Port)
-	// Set up the database
-	models.SetupDatabase(dsn)
+        dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+                dbConfig.Host, dbConfig.Username, dbConfig.Password, dbConfig.DBName, dbConfig.Port)
+        // Set up the database
+        models.SetupDatabase(dsn)
 
 
-	// Create a new contract handler
-	contractHandler := handlers.NewContractHandler(configuration)
+        // Create a new contract handler
+        contractHandler := handlers.NewContractHandler(configuration)
 
-	userWalletHandler := handlers.NewUserWalletHandler(km, configuration)
+        userWalletHandler := handlers.NewUserWalletHandler(km, configuration)
 
-	transactionHandler := handlers.NewTransactionHandler(km, configuration)
+        transactionHandler := handlers.NewTransactionHandler(km, configuration)
 
-	userHandler := handlers.NewUserHandler()
+        userHandler := handlers.NewUserHandler()
 
-	// Create a new gin router
-	router := gin.Default()
+        // Create a new gin router
+        router := gin.Default()
+        router.RedirectTrailingSlash = true
 
-	// Set up CORS middleware
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
-	router.Use(cors.New(config))
+        router.Use(middlewares.CORSMiddleware())
+        // Set up the routes
+        SetupRoutes(router, contractHandler, userWalletHandler, transactionHandler, userHandler)
 
-	// Set up the routes
-	SetupRoutes(router, contractHandler, userWalletHandler, transactionHandler, userHandler)
-
-	// Run the server
-	err := router.Run(":8080")
-	if err != nil {
-		log.Fatalf("Failed to start the server: %v", err)
-	}
+        // Run the server
+        err := router.Run(":8080")
+        if err != nil {
+                log.Fatalf("Failed to start the server: %v", err)
+        }
 }
